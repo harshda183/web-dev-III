98f054260a8e2e4ca28e6bc2087ef20c8e394768
Harshda <harshda183@gmail.com>
2026-08-12 10:28:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const math=require("./math.js"); //local module
+//const os=require("os"); //core module
+//const express=require("express"); //third party module
+
+import { add , subtract } from "./math.js"; //local module
+
+console.log(add(5, 10));
+console.log(subtract(10, 5));
+//console.log(os.ocpus().length); //core module
+
+//console.log(math.add(5, 10)); //local module
