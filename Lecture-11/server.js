73daa6114d8209374ea4fafc3d9edf0966ebea50
harshda183 <harshda183@gmail.com>
2026-09-10 13:45:44 +0530

    Update server.js
    Added a comment to clarify the purpose of express.json() middleware.

--- Lecture-11/server.js
+++ Lecture-11/server.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 const PORT=3000
-
+//
 app.use(express.json()); //it handles the json data coming from the client//encode
 
 const students=[
